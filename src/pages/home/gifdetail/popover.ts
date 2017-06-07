b4de7749a4aa2318e138c165ca43ab19035c2322
vinay13 <aitvinay@gmail.com>
2017-06-07 17:32:45 +0530

    popover logout dismiss

--- src/pages/home/gifdetail/popover.ts
+++ src/pages/home/gifdetail/popover.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {NavController,ToastController, ViewController } from 'ionic-angular';
+
 
 @Component({
     selector: 'page-popover',
@@ -7,5 +9,25 @@ import { Component } from '@angular/core';
 
 export class PopOverComponent {
 
-    constructor(){}
+    constructor(public navCtrl : NavController,
+                public toastCtrl : ToastController,
+                public viewCtrl: ViewController){
+
+
+
+                }
+
+     presentToast(){
+        let toast = this.toastCtrl.create({
+            message: 'You are now Logout',
+            duration: 3000
+        });
+        toast.present();
+    }
+
+    logout(){
+        this.presentToast();
+        this.viewCtrl.dismiss();
+    }
+   
 }
